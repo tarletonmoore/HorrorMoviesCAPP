b64db4d22862fd6550473c36ccc27773867206c1
Tarleton Moore <tarletonmoore@gmail.com>
2024-04-02 16:42:59 -0400

    gitignore

--- obj/Debug/net8.0/MyHorrorMovieApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyHorrorMovieApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyHorrorMovieApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a83fda043aebc56e3b460b91864ba655a1390fae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+691094666daf0d5e5748b966056908a2b6d4ebe6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyHorrorMovieApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyHorrorMovieApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
